08d14cee4e8423d22c105851e4cc7ec974183c6b
Saismita <saismitamohanty100@gmail.com>
2021-05-30 23:42:42 -0700

    javascript-2nd day

--- json.js
+++ json.js
@@ -7,5 +7,5 @@ var myDetails = {
 JSON.parse('{"key1": "value1", "key2": 20}')
 JSON.stringify(myDetails)
 console.log(myDetails);
-console.log(myDetails.age);
+
 console.log(myDetails['bloodGroup']);
